789ed3dad4cc40379ac268aefb34266bd13b6b06
scaldings <lukas.zahoransky@zsbystricany.info>
2020-11-24 22:15:53 +0100

    Added Gilded Netherite Armor

--- src/main/java/me/scaldings/gildednetherite/init/Items.java
+++ src/main/java/me/scaldings/gildednetherite/init/Items.java
@@ -1,5 +1,7 @@
 package me.scaldings.gildednetherite.init;
 
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -8,10 +10,27 @@ import net.minecraft.util.registry.Registry;
 public class Items
 {
     private static final String MOD_ID = "gildednetherite";
+
+    //Items
     public static final Item GILDED_NETHERITE_INGOT = new Item(new Item.Settings().fireproof().group(ItemGroup.MATERIALS));
+
+    //Armor
+    public static final Item GILDED_NETHERITE_HELMET = new ArmorItem(GildedNetheriteMaterial.GILDED_NETHERITE, EquipmentSlot.HEAD, new Item.Settings().fireproof().group(ItemGroup.COMBAT));
+    public static final Item GILDED_NETHERITE_CHESTPLATE = new ArmorItem(GildedNetheriteMaterial.GILDED_NETHERITE, EquipmentSlot.CHEST, new Item.Settings().fireproof().group(ItemGroup.COMBAT));
+    public static final Item GILDED_NETHERITE_LEGGINGS = new ArmorItem(GildedNetheriteMaterial.GILDED_NETHERITE, EquipmentSlot.LEGS, new Item.Settings().fireproof().group(ItemGroup.COMBAT));
+    public static final Item GILDED_NETHERITE_BOOTS = new ArmorItem(GildedNetheriteMaterial.GILDED_NETHERITE, EquipmentSlot.FEET, new Item.Settings().fireproof().group(ItemGroup.COMBAT));
 
     public static void registerItems()
     {
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, "gilded_netherite_ingot"), GILDED_NETHERITE_INGOT);
+        register(GILDED_NETHERITE_INGOT, "gilded_netherite_ingot");
+        register(GILDED_NETHERITE_HELMET, "gilded_netherite_helmet");
+        register(GILDED_NETHERITE_CHESTPLATE, "gilded_netherite_chestplate");
+        register(GILDED_NETHERITE_LEGGINGS, "gilded_netherite_leggings");
+        register(GILDED_NETHERITE_BOOTS, "gilded_netherite_boots");
+    }
+
+    private static void register(Item item, String name)
+    {
+        Registry.register(Registry.ITEM, new Identifier(MOD_ID, name), item);
     }
 }
