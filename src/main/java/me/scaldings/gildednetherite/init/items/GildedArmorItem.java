3ea564feb228409d57381b4a0815f8938d932958
scaldings <lukas.zahoransky@zsbystricany.info>
2020-11-30 00:08:00 +0100

    Remove redundant methods and fields

--- src/main/java/me/scaldings/gildednetherite/init/items/GildedArmorItem.java
+++ src/main/java/me/scaldings/gildednetherite/init/items/GildedArmorItem.java
@@ -2,18 +2,12 @@ package me.scaldings.gildednetherite.init.items;
 
 import me.scaldings.gildednetherite.init.materials.GildedArmorMaterial;
 import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
 
 public class GildedArmorItem extends ArmorItem
 {
-    private final EquipmentSlot slot;
     private final int protection;
     private final float toughness;
     private final ArmorMaterial type;
@@ -21,41 +15,17 @@ public class GildedArmorItem extends ArmorItem
     public GildedArmorItem(ArmorMaterial material, EquipmentSlot slot, Settings settings) {
         super(material, slot, settings);
         this.type = GildedArmorMaterial.GILDED;
-        this.slot = slot;
         this.protection = this.type.getProtectionAmount(slot);
         this.toughness = this.type.getToughness();
     }
 
-    public int getEnchantability() {
-        return this.type.getEnchantability();
-    }
+    public int getEnchantability() {return this.type.getEnchantability();}
 
-    public ArmorMaterial getMaterial() {
-        return this.type;
-    }
+    public ArmorMaterial getMaterial() {return this.type;}
 
-    public boolean canRepair(ItemStack stack, ItemStack ingredient) {
-        return this.type.getRepairIngredient().test(ingredient) || super.canRepair(stack, ingredient);
-    }
+    public boolean canRepair(ItemStack stack, ItemStack ingredient) {return this.type.getRepairIngredient().test(ingredient);}
 
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        EquipmentSlot equipmentSlot = MobEntity.getPreferredEquipmentSlot(itemStack);
-        ItemStack itemStack2 = user.getEquippedStack(equipmentSlot);
-        if (itemStack2.isEmpty()) {
-            user.equipStack(equipmentSlot, itemStack.copy());
-            itemStack.setCount(0);
-            return TypedActionResult.success(itemStack, world.isClient());
-        } else {
-            return TypedActionResult.fail(itemStack);
-        }
-    }
+    public int getProtection() {return this.protection;}
 
-    public int getProtection() {
-        return this.protection;
-    }
-
-    public float method_26353() {
-        return this.toughness;
-    }
+    public float method_26353() {return this.toughness;}
 }
